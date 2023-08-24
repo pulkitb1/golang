51d75ab7c8efb24fdcbc72555b95691c4261dd06
pulkitb1 <pulkitbatra34@gmail.com>
2023-08-25 01:06:55 +0530

    fix: conversion dir

--- 04conversion/main.go
+++ 04conversion/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	fmt.Println("Welcome to our watch app")
-	fmt.Println("Please rate our pizza between 1 and 5")
+	fmt.Println("Please rate our watch between 1 and 5")
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
 	numRating, err := strconv.ParseFloat(strings.TrimSpace(input), 64)
