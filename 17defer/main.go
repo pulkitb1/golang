1e1fadac88903d9449ce5ec67aff5f24d10299fe
pulkitb1 <pulkitbatra34@gmail.com>
2023-08-28 21:42:58 +0530

    feat: defer in go lang

---
+++ 17defer/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("one")
+	defer fmt.Println("two")
+	fmt.Println("yo")
+	myDefer()
+}
+
+func myDefer() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+}
